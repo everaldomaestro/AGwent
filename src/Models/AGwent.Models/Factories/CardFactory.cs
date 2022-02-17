0b696d84bd0ae825fc127167da261da13d2abfa2
everaldomaestro <everaldo.maestro@gmail.com>
2022-02-16 22:48:26 -0300

    add cast

--- src/Models/AGwent.Models/Factories/CardFactory.cs
+++ src/Models/AGwent.Models/Factories/CardFactory.cs
@@ -8,28 +8,12 @@ namespace AGwent.Models.Factories
     {
         public static List<UnitCard> GetUnitCards(IList<Card> cards)
         {
-            var unitCards = new List<UnitCard>();
-
-            var cardsFiltered = cards.Where(x => x.Type == TypeCard.UNIT).ToList();
-            foreach (var card in cardsFiltered)
-            {
-                unitCards.Add((UnitCard)card);
-            }
-
-            return unitCards;
+            return cards.Where(x => x.Type == TypeCard.UNIT).Cast<UnitCard>().ToList();
         }
 
         public static List<SpecialCard> GetSpecialCards(IList<Card> cards)
         {
-            var specialCard = new List<SpecialCard>();
-
-            var cardsFiltered = cards.Where(x => x.Type == TypeCard.SPECIAL).ToList();
-            foreach (var card in cardsFiltered)
-            {
-                specialCard.Add((SpecialCard)card);
-            }
-
-            return specialCard;
+            return cards.Where(x => x.Type == TypeCard.SPECIAL).Cast<SpecialCard>().ToList();
         }
     }
 }
